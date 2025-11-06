3b42ef5a110dad82156146dc80a99d1c7c8078d0
james <james@jmcdowall.com>
2025-11-06 22:37:02 +0000

    Fixed Port Moduel

--- modules/ports/outputs.tf
+++ modules/ports/outputs.tf
@@ -41,33 +41,35 @@ output "port3" {
 
 output "all_ports" {
   description = "List of all configured port details"
-  value = compact([
-    local.ports_count >= 1 && var.max_ports >= 1 ? {
-      slot         = 1
-      number       = local.port1_num
-      title        = local.port1_title
-      icon         = local.port1_icon
-      share        = local.port1_share
-      app_id       = coder_app.port1[0].id
-      url          = coder_app.port1[0].url
-    } : null,
-    local.ports_count >= 2 && var.max_ports >= 2 ? {
-      slot         = 2
-      number       = local.port2_num
-      title        = local.port2_title
-      icon         = local.port2_icon
-      share        = local.port2_share
-      app_id       = coder_app.port2[0].id
-      url          = coder_app.port2[0].url
-    } : null,
-    local.ports_count >= 3 && var.max_ports >= 3 ? {
-      slot         = 3
-      number       = local.port3_num
-      title        = local.port3_title
-      icon         = local.port3_icon
-      share        = local.port3_share
-      app_id       = coder_app.port3[0].id
-      url          = coder_app.port3[0].url
-    } : null,
-  ])
+  value = [
+    for port in [
+      local.ports_count >= 1 && var.max_ports >= 1 ? {
+        slot         = 1
+        number       = local.port1_num
+        title        = local.port1_title
+        icon         = local.port1_icon
+        share        = local.port1_share
+        app_id       = coder_app.port1[0].id
+        url          = coder_app.port1[0].url
+      } : null,
+      local.ports_count >= 2 && var.max_ports >= 2 ? {
+        slot         = 2
+        number       = local.port2_num
+        title        = local.port2_title
+        icon         = local.port2_icon
+        share        = local.port2_share
+        app_id       = coder_app.port2[0].id
+        url          = coder_app.port2[0].url
+      } : null,
+      local.ports_count >= 3 && var.max_ports >= 3 ? {
+        slot         = 3
+        number       = local.port3_num
+        title        = local.port3_title
+        icon         = local.port3_icon
+        share        = local.port3_share
+        app_id       = coder_app.port3[0].id
+        url          = coder_app.port3[0].url
+      } : null,
+    ] : port if port != null
+  ]
 }
