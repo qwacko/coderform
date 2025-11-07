864a011ad096aad00b6f3d1dc4fbc5a4c93cf124
james <james@jmcdowall.com>
2025-11-07 02:58:42 +0000

    pgweb 2 again

--- modules/postgres/main.tf
+++ modules/postgres/main.tf
@@ -414,22 +414,22 @@ resource "coder_agent" "pgweb2" {
   os   = "linux"
 
   # Note the single quotes around EOF: <<'EOF'
-  startup_script = <<'EOF'
-  #!/bin/sh
-  set -eux
+  startup_script = <<-EOT
+    #!/bin/sh
+    set -eux
 
-  PGWEB_VERSION=0.14.4
-  curl -fsSL -o /usr/local/bin/pgweb \
-    https://github.com/sosedoff/pgweb/releases/download/v${PGWEB_VERSION}/pgweb_linux_amd64
-  chmod +x /usr/local/bin/pgweb
+    PGWEB_VERSION=0.14.4
+    curl -fsSL -o /usr/local/bin/pgweb \
+      https://github.com/sosedoff/pgweb/releases/download/v${PGWEB_VERSION}/pgweb_linux_amd64
+    chmod +x /usr/local/bin/pgweb
 
-  # Start pgweb inside the agent. Using shell defaults like ${VAR:-...} is fine now.
-  pgweb --bind=127.0.0.1 --listen=8081 \
-    --host=postgres \
-    --user="${POSTGRES_USER:-postgres}" \
-    --pass="${POSTGRES_PASSWORD:-postgres}" \
-    --db="${POSTGRES_DB:-postgres}" &
-EOF
+    # Start pgweb inside the agent. Using shell defaults like ${VAR:-...} is fine now.
+    pgweb --bind=127.0.0.1 --listen=8081 \
+      --host=postgres \
+      --user="${POSTGRES_USER:-postgres}" \
+      --pass="${POSTGRES_PASSWORD:-postgres}" \
+      --db="${POSTGRES_DB:-postgres}" &
+  EOT
 }
 
 resource "coder_app" "pgweb2_host" {
